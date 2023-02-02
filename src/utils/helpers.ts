5e4d73e176a4b9690cbc65ba9716714fedab2b73
Refaelbenzvi24 <refaelbenzvi24@gmail.com>
2023-02-02 14:15:00 +0200

    replaced debounce function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,13 @@
-export function debounce<T extends Function>(cb: T, wait = 20) {
-	let h: NodeJS.Timeout
-	let callable = (...args: any) => {
-		clearTimeout(h);
-		h = setTimeout(() => cb(...args), wait);
-	};
-	return <T>(<any>callable);
+export function debounce<Params extends any[]>(
+	func: (...args: Params) => any,
+	timeout: number,
+): (...args: Params) => void {
+	let timer: NodeJS.Timeout
+	
+	return (...args: Params) => {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			func(...args)
+		}, timeout)
+	}
 }
